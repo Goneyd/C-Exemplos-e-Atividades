8ed5f70af7817121c474397754347113cb042ef9
Gabriel Schmidt <gabriel.schmidt@al.infnet.edu.br>
2025-03-27 09:05:44 -0300

    JoalheriaVP4

--- Aula C3#/joalheria/Program.cs
+++ Aula C3#/joalheria/Program.cs
@@ -6,6 +6,11 @@ namespace joalheria;
         static void Main(string[] args)
         {
             List<string> horarioSeguranca1 = ["18:00 - 02:00","12:00 - 20:00"];
+
+            void NovoFuncionario()
+            {
+                   
+            }
             
             var gerente1 = new Surpervisor(nome:"Gabriel",sobre_nome:"Schmidt",valor_hora:18.00,depedente: 0,turno:"Noite");
             var seguranca1 = new Seguranca(nome:"Diogo",sobre_nome:"Castilha",valor_hora:10.00,depedente: 2,horarios:horarioSeguranca1);
